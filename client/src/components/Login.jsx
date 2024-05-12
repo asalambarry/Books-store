bde28d9d4ece5f5fd455667f87ea1688a0e22522
Barry <a.salambarry@gmail.com>
2024-05-12 16:15:41 +0200

    creation formulaire login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,27 @@
+import '../styles/Login.css'
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className="login-page">
+            <div className="login-container">
+                <h2>Login</h2> <br />
+                <div className="form-group">
+                    <label htmlFor="username">Username : </label>
+                    <input type="text" name="" value="" placeholder="Enter Username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password : </label>
+                    <input type="password" name="" value="" placeholder="Enter Password" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Role : </label>
+                    <select name="role" id="role">
+                        <option value="admin">Admin</option>
+                        <option value="student">Student</option>
+                    </select>
+                </div>
+                <button type="" className="btn-login">Login</button>
+            </div>
+        </div>
+    )
 }
 export default Login
